eff8c04402b6ce0de966cbec858d1d87d798881e
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-11-21 14:02:38 -0300

    Adding google login

--- tcloud-webapp/components/info_card.tsx
+++ tcloud-webapp/components/info_card.tsx
@@ -28,7 +28,6 @@ export default (props:Props) => {
      </div>
     ))}
    
- 
     <style jsx>
       {
         ` .row {
--- tcloud-webapp/pages/index.tsx
+++ tcloud-webapp/pages/index.tsx
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default () => <div>Index</div>
+import { GoogleLogin } from 'react-google-login';
+
+export default () => {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId="398971931281-8r0rgrmtc5oqples56h0ims14b0giiec.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
+    </div>
+  )
+}
