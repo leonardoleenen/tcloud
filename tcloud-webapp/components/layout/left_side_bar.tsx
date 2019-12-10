54aa815f00992702a6dc2e1f59863cca521b44d4
imgermi <juanmartingermano@gmail.com>
2019-12-10 18:10:53 -0300

    fijo navegacion

--- tcloud-webapp/components/layout/left_side_bar.tsx
+++ tcloud-webapp/components/layout/left_side_bar.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 export default (props) => {
-    return <div className="p-5">
+    return <div className="p-5 h-screen LeftSideBar sticky top-0 overflow-auto">
         {props.children}
-        
-    </div> 
+    <style jsx>
+    	{
+    		`
+    			.LeftSideBar::-webkit-scrollbar {
+    			  display: none;
+    			}
+    		`
+    	}
+    </style>
+    </div>
 }
