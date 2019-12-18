0f52ccf8254fa0bd291a849e27f103c6c58aba82
imgermi <juanmartingermano@gmail.com>
2019-12-18 20:11:27 -0300

    ajustes UI

--- tcloud-webapp/pages/v/document_explorer.tsx
+++ tcloud-webapp/pages/v/document_explorer.tsx
@@ -36,8 +36,8 @@ export default () => {
   const [lastUpdate, setLastUpdate] = useState<number>(null)
   const [textToSearch, setTextToSearch] = useState(null)
 
-  const tabSelectedStyle = 'bg-indigo-500 px-8 py-2 rounded-lg text-sm font-semibold text-white hover:bg-indigo-700'
-  const tabNotSelectedStyle='border border-indigo-500 px-8 py-2 rounded-lg text-sm font-semibold text-indigo-500 hover:border-indigo-700 hover:text-indigo-700'
+  const tabSelectedStyle = 'px-12 py-3 text-sm font-semibold text-indigo-500 uppercase border-b-2 border-indigo-500 text-center'
+  const tabNotSelectedStyle='px-12 py-3 text-sm font-semibold text-gray-600 uppercase border-b-2 border-gray-300 text-center hover:opacity-75'
   
   useEffect(() => {
       var myWorker = new Worker("/worker.js");
@@ -115,7 +115,7 @@ export default () => {
   return (<div className="flex">
     <main className="w-full clearfix ">
 
-      <header className="h-16 flex items-center">
+      <header className="h-16 flex items-center bg-white sticky top-0 border-b-2 border-gray-200">
         <div className="bg-gray-300 flex items-center focus:shadow-outline border border-gray-300 rounded-lg   mx-4 py-2 px-4 block w-1/3">
         <div><IconSearch/></div>
         <input 
@@ -129,14 +129,18 @@ export default () => {
         </div>
       </header>
 
-      <article className="h-screen bg-gray-200 shadow">
-        <div className="mx-4 flex items-center m-auto pt-4 relative">
+      <article className="min-h-screen bg-gray-200 shadow px-6 py-8">
+        <div className="flex justify-between">
+          <h2 className= "text-2xl font-bold text-gray-800">Documentos</h2>
+          <label className="text-right">
+            <p className='text-gray-600 text-sm'>Última actualización</p>
+            <p className='text-gray-800 text-sm font-semibold'>{moment(lastUpdate).format('h:mm:ssA')}</p>
+          </label>
+        </div>
+        <div className="flex items-center m-auto pt-4 relative">
           <button className={`focus:outline-none  ${tabActive === TabActive.processed ? tabSelectedStyle : tabNotSelectedStyle}`} onClick={ () => setTabActive(TabActive.processed)}>Procesados</button>
-          <button className={`ml-4 focus:outline-none  ${tabActive === TabActive.pending ? tabSelectedStyle : tabNotSelectedStyle}`}  onClick={ () => setTabActive(TabActive.pending)}>En Proceso</button>
-          <button className={`ml-4 focus:outline-none  ${tabActive === TabActive.withError ? tabSelectedStyle : tabNotSelectedStyle}`}   onClick={ () => setTabActive(TabActive.withError)}>Con Errores</button>
-          <div className="absolute right-0">
-              <label className='text-gray-500 text-base text-xs font-thin' >Ultima Actualización {moment(lastUpdate).format('h:mm:ssA')}</label>
-          </div>
+          <button className={`focus:outline-none  ${tabActive === TabActive.pending ? tabSelectedStyle : tabNotSelectedStyle}`}  onClick={ () => setTabActive(TabActive.pending)}>En Proceso</button>
+          <button className={`focus:outline-none  ${tabActive === TabActive.withError ? tabSelectedStyle : tabNotSelectedStyle}`}   onClick={ () => setTabActive(TabActive.withError)}>Con Errores</button>
         </div>
 
 
@@ -144,7 +148,7 @@ export default () => {
 
 
        {tabActive === TabActive.processed ? <div>
-        <header className="flex pt-8 mx-4">
+        <header className="flex pt-8">
           <div className="w-2/3">
             <span className="text-gray-500 font-semibold text-sm">Identificador</span>
           </div>
@@ -161,20 +165,20 @@ export default () => {
 
         { filterDocuments()
               .map( d => (
-          <div className="flex bg-white mx-4 h-12 items-center mt-4 rounded-lg hover:bg-green-200 hover:shadow" key={d.document_id}>
+          <div className="flex bg-white h-16 items-center mt-4 rounded-lg hover:bg-green-100 hover:shadow" key={d.document_id}>
             <div className="w-2/3 pl-4">
               <Link href={`/v?documentId=${d.document_id}`}>
-                <span className="text-gray-700 font-semibold text-sm">{d.reference}</span>
+                <span className="text-gray-700 font-semibold text-sm cursor-pointer">{d.reference}</span>
               </Link>
             </div>
             <div className="w-64">
-              <span className="text-gray-700 font-semibold text-sm">{moment(d.processed_at).format(FORMAT_DATETIME)}</span>
+              <span className="text-gray-600 text-sm">{moment(d.processed_at).format(FORMAT_DATETIME)}</span>
             </div>
             <div className="w-32">
-              <span className="text-gray-700 font-semibold text-sm">{ moment(moment(d.processed_at).diff(moment(d.upload_date))).format("mm:ss")}</span>
+              <span className="text-gray-600 text-sm">{ moment(moment(d.processed_at).diff(moment(d.upload_date))).format("mm:ss")}</span>
             </div>
             <div className="w-32">
-              <span className="text-gray-800 font-semibold text-sm"><IconDownload /></span>
+              <span className="text-gray-800 text-sm cursor-pointer hover:opacity-75"><IconDownload /></span>
             </div>
           </div>
         ))}
@@ -187,7 +191,7 @@ export default () => {
 
 
         {tabActive === TabActive.pending ? <div>
-          <header className="flex pt-8 mx-4">
+          <header className="flex pt-8">
             <div className="w-2/3">
               <span className="text-gray-500 font-semibold text-sm">Identificador</span>
             </div>
@@ -200,7 +204,7 @@ export default () => {
             
           </header>
           {filterDocuments().filter( d => d.status === 'PENDING').map( docPending => ( 
-            <div className="flex bg-white mx-4 h-12 items-center mt-4 rounded-lg hover:bg-yellow-200 hover:shadow" key={docPending.job_id}>
+            <div className="flex bg-white h-16 items-center mt-4 rounded-lg hover:bg-yellow-200 hover:shadow" key={docPending.job_id}>
               <div className="w-2/3 pl-4">
                 <span className="text-gray-700 font-semibold text-sm">{docPending.reference}</span>
               </div>
@@ -219,8 +223,8 @@ export default () => {
 
 
         {tabActive === TabActive.withError ? <div>
-          <header className="flex pt-8 mx-4">
-            <div className="w-2/3">
+          <header className="flex pt-8">
+            <div className="w-7/12">
               <span className="text-gray-500 font-semibold text-sm">Identificador</span>
             </div>
             <div className="w-64">
@@ -235,18 +239,18 @@ export default () => {
             
           </header>
           {filterDocuments().filter( d => d.status === 'FAIL').map( docError => ( 
-          <div className="flex bg-white mx-4 mim-h-12 items-center mt-4 rounded-lg hover:bg-red-100 hover:shadow"  key={docError.job_id}>
-            <div className="w-2/3 pl-4">
+          <div className="flex bg-white h-16 items-center mt-4 rounded-lg hover:bg-red-100 hover:shadow"  key={docError.job_id}>
+            <div className="w-7/12 pl-4">
               <span className="text-gray-700 font-semibold text-sm">{docError.reference}</span>
             </div>
             <div className="w-64">
-              <span className="text-gray-500 font-semibold text-sm">{moment(docError.upload_date).format(FORMAT_DATETIME)}</span>
+              <span className="text-gray-600 text-sm">{moment(docError.upload_date).format(FORMAT_DATETIME)}</span>
             </div>
             <div className="w-48">
-              <span className="text-gray-500 font-semibold text-sm">{ moment(moment(docError.processed_at).diff(moment(docError.upload_date))).format("mm:ss")}</span>
+              <span className="text-gray-600 text-sm">{ moment(moment(docError.processed_at).diff(moment(docError.upload_date))).format("mm:ss")}</span>
             </div>
-            <div className="w-64 mr-4">
-              <span className="text-gray-700 font-semibold text-sm">{docError.info}</span>
+            <div className="w-64">
+              <span className="text-gray-600 text-sm">{docError.info}</span>
             </div>
             
           </div>))} 
@@ -257,14 +261,15 @@ export default () => {
 
       </article>
       <Link href="/v/load_file">
-        <div className=" flex items-center m-auto shadow absolute bottom-0 right-0 h-12 w-12 bg-indigo-500 mr-24 mb-8 rounded-full"> <IconAdd /> </div>
+        <div className="cursor-pointer flex items-center m-auto shadow fixed bottom-0 right-0 h-12 w-12 bg-indigo-500 mr-24 mb-8 rounded-full hover:opacity-75"> <IconAdd /> </div>
       </Link>
     </main>
 
-    <aside className="w-16 ">
-      <div className="h-8 w-8 mt-4 bg-indigo-100 m-auto flex items-center rounded-lg"><IconPeople /></div>
-      <div className="h-8 w-8 mt-4 bg-indigo-100 m-auto flex items-center rounded-lg"><IconNotification /></div>
-      
+    <aside className="w-16 bg-white border-l-2 border-gray-200">
+    <div className="sticky top-0 pt-4">
+      <div className="h-8 w-8 bg-indigo-100 m-auto flex items-center justify-center rounded-lg cursor-pointer"><IconPeople /></div>
+      <div className="h-8 w-8 mt-4 bg-indigo-100 m-auto flex items-center rounded-lg cursor-pointer"><IconNotification /></div>
+    </div>
     </aside>
 
   </div>
@@ -273,12 +278,11 @@ export default () => {
 }
 
 const IconDownload = () => (
-  <svg width="26" height="20" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M24.4843 13V17C24.4843 17.5304 24.212 18.0391 23.7273 18.4142C23.2427 18.7893 22.5853 19 21.8998 19H3.80859C3.12314 19 2.46577 18.7893 1.98109 18.4142C1.49641 18.0391 1.22412 17.5304 1.22412 17V13" stroke="#667EEA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M6.39331 8L12.8545 13L19.3156 8" stroke="#667EEA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M12.8542 13V1" stroke="#667EEA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+  <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19.2241 13V17C19.2241 17.5304 19.0134 18.0391 18.6383 18.4142C18.2633 18.7893 17.7546 19 17.2241 19H3.22412C2.69369 19 2.18498 18.7893 1.80991 18.4142C1.43483 18.0391 1.22412 17.5304 1.22412 17V13" stroke="#667EEA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M5.22437 8L10.2244 13L15.2244 8" stroke="#667EEA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M10.2241 13V1" stroke="#667EEA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
-
 )
 
 const IconNotification = () => (
@@ -291,19 +295,11 @@ const IconNotification = () => (
 
 
 const IconPeople = () => (
-  <svg style={{ margin: 'auto' }} width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g id="Icon/Outline/person">
-      <path id="Mask" fillRule="evenodd" clipRule="evenodd" d="M14 7C14 5.897 13.103 5 12 5C10.897 5 10 5.897 10 7C10 8.103 10.897 9 12 9C13.103 9 14 8.103 14 7ZM16 7C16 9.206 14.206 11 12 11C9.794 11 8 9.206 8 7C8 4.794 9.794 3 12 3C14.206 3 16 4.794 16 7ZM5 20C5 16.14 8.141 13 12 13C15.859 13 19 16.14 19 20C19 20.552 18.553 21 18 21C17.447 21 17 20.552 17 20C17 17.243 14.757 15 12 15C9.243 15 7 17.243 7 20C7 20.552 6.553 21 6 21C5.447 21 5 20.552 5 20Z" fill="#231F20" />
-      <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="5" y="3" width="14" height="18">
-        <path id="Mask_2" fillRule="evenodd" clipRule="evenodd" d="M14 7C14 5.897 13.103 5 12 5C10.897 5 10 5.897 10 7C10 8.103 10.897 9 12 9C13.103 9 14 8.103 14 7ZM16 7C16 9.206 14.206 11 12 11C9.794 11 8 9.206 8 7C8 4.794 9.794 3 12 3C14.206 3 16 4.794 16 7ZM5 20C5 16.14 8.141 13 12 13C15.859 13 19 16.14 19 20C19 20.552 18.553 21 18 21C17.447 21 17 20.552 17 20C17 17.243 14.757 15 12 15C9.243 15 7 17.243 7 20C7 20.552 6.553 21 6 21C5.447 21 5 20.552 5 20Z" fill="white" />
-      </mask>
-      <g mask="url(#mask0)">
-        <g id="&#240;&#159;&#142;&#168; Color">
-          <rect id="Base" width="24" height="24" fill="#5E81F4" />
-        </g>
-      </g>
-    </g>
-  </svg>)
+  <svg width="15" height="21" viewBox="0 0 15 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M14 20C14 15.0294 11.0899 11 7.5 11C3.91015 11 1 15.0294 1 20" stroke="#5E81F4" stroke-width="1.56098" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M7.5 8C9.433 8 11 6.433 11 4.5C11 2.567 9.433 1 7.5 1C5.567 1 4 2.567 4 4.5C4 6.433 5.567 8 7.5 8Z" stroke="#5E81F4" stroke-width="1.56098" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+)
 
 
 const IconAdd = () => (
